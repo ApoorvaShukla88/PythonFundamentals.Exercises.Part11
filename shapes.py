61f12e2014bb93ef3a2dcdd034856ab541a0cd18
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-15 11:24:32 -0400

    changes

--- shapes.py
+++ shapes.py
@@ -1,19 +1,31 @@
 class Rectangle:
     def __init__(self, length, width):
-        self.l = length
-        self.w = width
+        self.length = length
+        self.width = width
 
-        def area():
-            return 2*length*width
+    def area(self):
+        ar = self.length * self.width
+        print(ar)
 
-        def perimeter():
-            return 2*(length+width)
+    def perimeter(self):
+        peri = 2 * (self.length + self.width)
+        print(peri)
 
 
 class Square(Rectangle):
-    rect = shapes.Rectangle(2, 4)
-    rect.area()
+
+    def __init__(self, width):
+
+        self.width = width
+        self.length = width
 
 
 
-
+rect = Rectangle(2, 4)
+rect.area()
+#8
+square = Square(8)
+square.area()
+#64
+square.perimeter()
+#32
